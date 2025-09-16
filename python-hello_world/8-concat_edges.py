db23fc3cb234237478a57a54106c4d4da169125f
gastonvargas21 <gastonvargas1238@gmail.com>
2025-09-16 17:40:57 +0000

    lala

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[33:61] + str[102:107] + str[:6])
+print(str[39:67] + str[107:113] + str[:6])
