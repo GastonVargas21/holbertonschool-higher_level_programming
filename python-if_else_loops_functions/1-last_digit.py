cd8eae630839a9898ad7f992a57892453d5be98b
gastonvargas21 <gastonvargas1238@gmail.com>
2025-09-19 14:35:19 +0000

    la

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+import random
+number = random.randint(-10000, 10000)
+ultimo_digito = abs(number) % 10
+print(f"El último dígito de {number} es {ultimo_digito}", end='')
+if ultimo_digito > 5:
+    print(" y es mayor que 5")
+elif ultimo_digito == 0:
+    print(" y es 0")
+else:
+    print(" y es menor que 6 y no es 0")
