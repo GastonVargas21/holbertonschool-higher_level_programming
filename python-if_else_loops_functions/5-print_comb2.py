a908b399d3b0a20c42535e7d686b6cc4c8d3c1f8
gastonvargas21 <gastonvargas1238@gmail.com>
2025-09-19 19:12:47 +0000

    aha

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02}".format(i) if i == 99 else "{:02}, ".format(i), end="\n" if i == 99 else "")
+    print(
+        "{:02}".format(i) if i == 99 else "{:02}, ".format(i),
+        end="\n" if i == 99 else ""
+    )
