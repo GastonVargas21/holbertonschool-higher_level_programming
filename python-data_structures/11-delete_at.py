9947d5b381cb952d81b046f4db50acc39da1a123
gastonvargas21 <gastonvargas1238@gmail.com>
2025-10-01 20:30:34 +0000

    hh

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,6 +4,5 @@ def delete_at(my_list=[], idx=0):
         return my_list
     for i in range(idx, len(my_list) - 1):
         my_list[i] = my_list[i + 1]
-    my_list.pop()  
     del my_list[-1]
     return my_list
