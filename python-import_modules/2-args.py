2f5ecc78333146d6b8536b87244056e768437e4f
gastonvargas21 <gastonvargas1238@gmail.com>
2025-10-01 14:43:04 +0000

    aaaaaa

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-args = sys.argv[1:]
-count = len(args)
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+    if count == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(count, "" if count == 1 else "s"))
+        for i in range(count):
+            print("{}: {}".format(i + 1, args[i]))
 
-if count == 0:
-    print("0 arguments.")
-else:
-    print("{} argument{}:".format(count, "" if count == 1 else "s"))
-    for i in range(count):
-        print("{}: {}".format(i + 1, args[i])
