d9e7e742b3a00edbc5716c5b1accad6482c37c9a
gastonvargas21 <gastonvargas1238@gmail.com>
2025-10-01 20:24:15 +0000

    aa

---
+++ python-data_structures/10-main.py
@@ -0,0 +1,2 @@
+my_list = [0, 1, 2, 3, 4, 5, 6]
+print(divisible_by_2(my_list))
---
+++ python-data_structures/9-main.py
@@ -0,0 +1,2 @@
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+print("Max: {}".format(max_integer(my_list)))  # Salida: Max: 90
